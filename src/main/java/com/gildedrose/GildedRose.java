5e877728aa8365c9997ba80411c28fe05a76b61b
gchalas <gchalas@vega-systems.com>
2019-01-08 14:38:57 +0100

    refacto complete

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,7 +3,6 @@ package com.gildedrose;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
-
 public class GildedRose {
     Item[] items;
 
@@ -13,48 +12,48 @@ public class GildedRose {
 
     // method which update item quality
     public void updateQuality() {
+
+        // Method who set the specificity of the items
+        for (Item item : items) {
+            item.specific = item.setSpecific(item);
+            item.quality = item.sulfurasQuality(item);
+        }
+
         final Logger logger = LoggerFactory.getLogger(GildedRose.class);
         for (Item item : items) {
             if (item.quality >= 0 && item.quality <= 50) {
-
                 //Downgrade items sellIn
                 if (!item.name.contains("Sulfuras")) {
                     item.sellIn--;
                 }
+                if (item.specific) {
 
-                // quality if sell in > 0
-                if (item.sellIn >= 0) {
-                    item.quality = positiveSellIn(item);
+                    // Update quality of Brie
+                    item.quality = item.agedBrieQuality(item);
+
+                    //Update Quality of conjured items
+                    item.quality = item.conjuredQuality(item);
+
+                    //Update quality of passes
+                    item.quality = item.passesQuality(item);
                 } else {
-                    // quality if sell in < 0
-                    item.quality = negativeSellIn(item);
+                    // quality if sell in > 0
+                    if (item.sellIn >= 0) {
+                        item.quality = positiveSellIn(item);
+                    } else {
+                        // quality if sell in < 0
+                        item.quality = negativeSellIn(item);
+                    }
                 }
-
-                // Update quality of Brie
-                item.quality = agedBrieQuality(item);
-
-                //Set quality of Sulfuras
-                item.quality = sulfurasQuality(item);
-
-                //Update Quality of conjured items
-                item.quality = conjuredQuality(item);
-
-                //Update quality of passes
-                item.quality = passesQuality(item);
+                item.quality = correctionQuality(item);
+                displayLog(item, logger);
             }
-            item.quality = correctionQuality(item);
-            catchException(item, logger);
         }
-    }
-
-    public Item[] getItems() {
-        return items;
     }
 
     //General method when the item sell in is positive
     private int positiveSellIn(Item item) {
-        if (!item.name.contains("Backstage passes") && !item.name.equals("Sulfuras, Hand of Ragnaros")
-                && !item.name.equals("Conjured Mana Cake") && !item.name.equals("Aged Brie") && item.quality > 0) {
+        if (item.quality > 0) {
             return item.quality = item.quality - 1;
         } else {
             return item.quality;
@@ -63,71 +62,14 @@ public class GildedRose {
 
     //General method when the item sell in is negative
     private int negativeSellIn(Item item) {
-        if (!item.name.contains("Backstage passes") && !item.name.equals("Sulfuras, Hand of Ragnaros")
-                && !item.name.equals("Conjured Mana Cake") && !item.name.equals("Aged Brie") && item.quality > 1) {
+        if (item.quality > 1) {
             return item.quality = item.quality - 2;
         } else {
             return item.quality;
         }
     }
 
-    // Method which update brie quality
-    private int agedBrieQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            if (item.sellIn >= 0) {
-                return item.quality + 1;
-            } else {
-                return item.quality + 2;
-            }
-        }
-        return item.quality;
-    }
-
-    // Method which update conjured item quality
-    private int conjuredQuality(Item item) {
-        if (item.name.equals("Conjured Mana Cake")) {
-            if (item.sellIn >= 0) {
-                return item.quality = item.quality - 2;
-            }
-            if (item.sellIn < 0) {
-                return item.quality = item.quality - 4;
-            }
-        }
-        if (item.name.equals("Conjured Mana Cake") && item.quality == 1) {
-            return item.quality = 0;
-        }
-        return item.quality;
-    }
-
-    // Method which update passes quality
-    private int passesQuality(Item item) {
-        if (item.name.contains("Backstage passes")) {
-            if (item.sellIn < 11) {
-                if (item.sellIn >= 6 && item.quality <= 48) {
-                    return item.quality = item.quality + 2;
-                }
-                if (item.sellIn < 6 && item.quality <= 47) {
-                    return item.quality = item.quality + 3;
-                }
-            }
-            if (item.sellIn < 0) {
-                return item.quality = 0;
-            } else {
-                return item.quality = item.quality + 1;
-            }
-        }
-        return item.quality;
-    }
-
-    //Method which set sulfuras quality
-    private int sulfurasQuality(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return item.quality = 80;
-        }
-        return item.quality;
-    }
-
-    private void catchException(Item item, Logger logger) {
+    private void displayLog(Item item, Logger logger) {
         if (item.quality < 0) {
             logger.debug("Quality of item " + item.name + " is under 0");
         }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -8,14 +8,81 @@ public class Item {
 
     public int quality;
 
+    public boolean specific;
+
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    // Method which set specific attribute for the item
+    boolean setSpecific(Item item) {
+        if (item.name.contains("Backstage passes") || item.name.equals("Sulfuras, Hand of Ragnaros")
+                || item.name.startsWith("Conjured") || item.name.equals("Aged Brie")) {
+            return item.specific = true;
+        } else {
+            return item.specific = false;
+        }
+    }
+
+    // Method which update brie quality
+    int agedBrieQuality(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            if (item.sellIn >= 0) {
+                return item.quality + 1;
+            } else {
+                return item.quality + 2;
+            }
+        }
+        return item.quality;
+    }
+
+    // Method which update conjured item quality
+    int conjuredQuality(Item item) {
+        if (item.name.startsWith("Conjured")) {
+            if (item.sellIn >= 0) {
+                return item.quality = item.quality - 2;
+            } else {
+                return item.quality = item.quality - 4;
+            }
+        }
+        if (item.name.startsWith("Conjured") && item.quality == 1) {
+            return item.quality = 0;
+        }
+        return item.quality;
+    }
+
+    // Method which update passes quality
+    int passesQuality(Item item) {
+        if (item.name.contains("Backstage passes")) {
+            if (item.sellIn < 11) {
+                if (item.sellIn >= 6 && item.quality <= 48) {
+                    return item.quality = item.quality + 2;
+                }
+                if (item.sellIn < 6 && item.quality <= 47) {
+                    return item.quality = item.quality + 3;
+                }
+            }
+            if (item.sellIn < 0) {
+                return item.quality = 0;
+            } else {
+                return item.quality = item.quality + 1;
+            }
+        }
+        return item.quality;
+    }
+
+    //Method which set sulfuras quality
+    int sulfurasQuality(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return item.quality = 80;
+        }
+        return item.quality;
     }
 }
