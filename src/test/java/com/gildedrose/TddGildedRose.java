e431bf2050f688e83c4146243042ce66d8de31b0
gchalas <gchalas@vega-systems.com>
2019-01-09 12:19:05 +0100

    test shouldDecreaseTwiceWhenSellInIsUnderZero

--- src/test/java/com/gildedrose/TddGildedRose.java
+++ src/test/java/com/gildedrose/TddGildedRose.java
@@ -1,22 +1,25 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TddGildedRose {
 
-    private GildedRose mainGilded;
-    private Item[] items;
-
-    public TddGildedRose() {
-        Item testItem = new Item("chievre",1,1);
-        this.items = new Item[]{testItem};
-        this.mainGilded = new GildedRose(items);
+    @Test
+    public void QualityShouldDecreaseWhenSellInDecrease() {
+        Item testItem = new Item("Chievre", 1, 1);
+        GildedRose mainGilded = new GildedRose(new Item[]{testItem});
+        mainGilded.updateQuality();
+        assertThat(new Item[]{testItem}[0]).isEqualToComparingFieldByField(new Item("Chievre", 0, 0));
     }
 
     @Test
-    public void QualityShouldDecreaseWhenSellInDecrease() {
+    public void shouldDecreaseTwiceWhenSellInIsUnderZero() {
+        Item testItem = new Item("Chievre", -5, 2);
+        GildedRose mainGilded = new GildedRose(new Item[]{testItem});
         mainGilded.updateQuality();
-        assertThat(items[0]).isEqualToComparingFieldByField(new Item("chievre",0,0));
+
+        assertThat(new Item[]{testItem}[0]).isEqualToComparingFieldByField(new Item("Chievre", -6, 0));
     }
 }
