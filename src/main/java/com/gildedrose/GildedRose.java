3f58c303d6de5c5f6619af485c6edcd04753790c
gchalas <gchalas@vega-systems.com>
2019-01-09 13:57:39 +0100

    code Sulfuras

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,31 +10,32 @@ public class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            item.sellIn--;
-            if (!item.name.equals("Aged Brie")) {
-                decreaseQuality(item);
-                if (item.sellIn < 0) {
+            if (!item.name.equals("Sulfuras")) {
+                item.sellIn--;
+                if (!item.name.equals("Aged Brie")) {
                     decreaseQuality(item);
-                }
-                if (item.quality < 0) {
-                    item.quality = 0;
-                }
-            }
-            else{
-                increaseQuality(item);
-                if(item.sellIn<0){
+                    if (item.sellIn < 0) {
+                        decreaseQuality(item);
+                    }
+                    if (item.quality < 0) {
+                        item.quality = 0;
+                    }
+                } else {
                     increaseQuality(item);
+                    if (item.sellIn < 0) {
+                        increaseQuality(item);
+                    }
                 }
             }
         }
     }
 
     public void decreaseQuality(Item item) {
-            item.quality--;
+        item.quality--;
     }
 
     public void increaseQuality(Item item) {
-        if(item.quality<50) {
+        if (item.quality < 50) {
             item.quality++;
         }
     }
